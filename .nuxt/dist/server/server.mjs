73aa73372b1b27289c43f9b42c48b00eb878f042
vdevcode <thanhvumaidao1234567@gmail.com>
2024-06-15 12:47:06 +0700

    fix: setup components nuxt3

---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/home/Desktop/minano-romanji-japan/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- nuxt.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  target: 'static', // Or 'server' if using SSR
-  buildDir: 'dist',
-  buildModules: [
-    '@nuxtjs/tailwindcss',
-  ],
-}
